20a2c0153486edaf0faf8a6dbefb2bd038e55d5a
durranee <durranee@hotmail.com>
2018-02-12 18:58:10 +0000

    Ex 2. method displaying names starting with added.

--- directory.rb
+++ directory.rb
@@ -1,4 +1,4 @@
-students = [
+=begin
   { name: 'Dr. Hannibal Lecter', cohort: :november },
   { name: 'Darth Vader', cohort: :november },
   { name: 'Nurse Ratched', cohort: :november },
@@ -11,20 +11,56 @@ students = [
   { name: 'Joffrey Baratheon', cohort: :november },
   { name: 'Norman Bates', cohort: :november }
 ]
+=end
+
+
+def input_students
+  students = []
+
+  puts 'Enter names of the students'
+  puts 'To finish, hit enter twice'
+  name = '-'
+  until name.empty?
+    name = gets.chomp
+    students << { name: name, cohort: :november } unless name.empty?
+    puts "Now we have #{students.count} students"
+  end
+  students
+end
 
 def print_header
   puts 'Students of Villian Academy'
   puts '---------------------------'
 end
 
-def print (students)
-  students.each { |student| puts "#{student[:name]} (#{student[:cohort]} cohort)" }
+def print_name_starting_with(students, letter)
+  student_names_with = []
+  students.each { |student| student_names_with << student if student[:name].start_with?(letter) }
+  student_names_with.each_with_index do |student, index|
+    puts "#{index + 1}. #{student[:name]} (#{student[:chort]} cohort)"
+  end
+
+  # redundant code below (prints without storing in array)
+  # students.each_with_index do |student, index|
+  #  puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)" if student[:name][0].start_with?(letter)
+  # end
 end
 
-def print_footer (students)
+def print(students)
+  students.each_with_index do |student, index|
+    puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
+  end
+end
+
+def print_footer(students)
   puts "Overall we have #{students.count} great students"
 end
 
+students = []
+students = input_students
 print_header
-print(students)
+puts 'Enter letter you wanna see students name starting from'
+letter = gets.chomp
+print_name_starting_with(students,letter)
+#print(students)
 print_footer(students)
