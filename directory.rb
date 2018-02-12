d403334940b640415c26a4a0a44ecb6f71da61bd
durranee <durranee@hotmail.com>
2018-02-12 19:48:05 +0000

    modified the input_students method and new method print_all_with_details Ex 5

--- directory.rb
+++ directory.rb
@@ -3,12 +3,24 @@
 def input_students
   students = []
 
-  puts 'Enter names of the students'
-  puts 'To finish, hit enter twice'
+
   name = '-'
   until name.empty?
+    puts 'Enter names of the students'
+    puts 'To finish, hit enter twice'
     name = gets.chomp
-    students << { name: name, cohort: :november } unless name.empty?
+    break if name.empty?
+    p "Enter Hobbies: "
+    hobbies = gets.chomp
+    p 'Enter country: '
+    country = gets.chomp
+    p 'Enter date of birth: '
+    dob = gets.chomp
+    p 'Enter height: '
+    height = gets.chomp
+
+    students << { name: name, cohort: :november, hobbies: hobbies,
+                  country: country, dob: dob, height: height }
     puts "Now we have #{students.count} students"
   end
   students
@@ -20,12 +32,29 @@ def print_header
   puts '---------------------------'
 end
 
+# method to print footer
+def print_footer(students)
+  puts "Overall we have #{students.count} great students"
+end
+
 # method to print all students names and cohorts
 def print(students)
   students.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
+
+# method to print all the details of students
+def print_with_all_details(students)
+  students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+    puts "Hobbies: #{student[:hobbies]}"
+    puts "Country: #{student[:country]}"
+    puts "Date of birth: #{student[:dob]}"
+    puts "Height: #{student[:height]}"
+  end
+end
+
 
 # method to print all students names with number in front of them
 def print_with_numbers(students)
@@ -57,17 +86,21 @@ def print_name_shorter_than_12_chars(students)
   end
 end
 
-
-# method to print footer
-def print_footer(students)
-  puts "Overall we have #{students.count} great students"
+def print_all_without_each(students)
+  index = 0
+  while index < students.length
+    puts "#{index + 1}. #{students[index][:name]} (#{students[index][:cohort]} cohort)"
+    index += 1
+  end
 end
 
 students = []
 students = input_students
 print_header
-print_name_starting_with(students,'a')
-print(students)
-print_with_numbers(students)
-print_name_shorter_than_12_chars(students)
+#print_name_starting_with(students,'a')
+# print(students)
+print_with_all_details(students)
+#print_with_numbers(students)
+#print_name_shorter_than_12_chars(students)
+#print_all_without_each(students)
 print_footer(students)
